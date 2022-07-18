7ca6254e8153bed1493226df5e1800c8dec32d74
tetra12 <socmicro@yandex.com>
2022-07-18 18:36:08 +0300

    feat: integrate greeter with front

--- front/src/counter.ts
+++ front/src/counter.ts
@@ -1,9 +1,23 @@
+import { HelloRequest } from "./proto/greeter/greeter";
+import greeter from "./client";
+
+const greet = () => {
+  const req: HelloRequest = { name: "Blah" };
+
+  const res = greeter.sayHello(req);
+
+  console.log(`*** Server Response Code: ${res.status}`);
+};
+
 export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
+  let counter = 0;
   const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(++counter))
-  setCounter(0)
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+
+  greet();
+
+  element.addEventListener("click", () => setCounter(++counter));
+  setCounter(0);
 }
